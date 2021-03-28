a33805d12268e74c9a3a6f86e3890d5076c07751
Jacob Riley <jacobriley1987@gmail.com>
2021-03-28 15:54:12 -0700

    connecction.js update/heroku connection/added to json

--- config/conncection.js
+++ config/conncection.js
@@ -1,14 +1,27 @@
+let mysql = require("mysql");
+// const db = require("db");
+let connection;
+// var mysql = require("mysql");
 
-var mysql = require("mysql");
+// var connection = mysql.createConnection({
+  
+// });
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Benson2387!",
-  database: "burger_db",
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Benson2387!",
+    database: "burger_db",
+    // host: process.env.DB_HOST,
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: burger_db,
+  });
+}
 
 connection.connect(function (err) {
   if (err) {
